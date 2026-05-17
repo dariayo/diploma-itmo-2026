c6f0bd3dcc764868361a97b35f0516b08a5db883
Шевченко Дарья <d.shevchenko@rivc-pulkovo.ru>
2026-05-17 23:20:44 +0300

    feat: add references

--- backend/src/main/java/com/diploma/DiplomaItmoApplication.java
+++ backend/src/main/java/com/diploma/DiplomaItmoApplication.java
@@ -3,11 +3,9 @@ package com.diploma;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class DiplomaItmoApplication {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(DiplomaItmoApplication.class, args);
     }
 }
